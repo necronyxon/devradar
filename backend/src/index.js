c46b25bf5c0f996eb3540217cf887c1b869f482b
Rafael Herzog de Oliveira <rafaelvys@gmail.com>
2020-01-15 17:08:39 -0300

    interface web pronta

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const routes = require('./routes')
 
 const app = express()
@@ -10,17 +11,8 @@ mongoose.connect('mongodb://omnistack:omnistack@cluster0-shard-00-00-brisu.mongo
     useCreateIndex: true,
 })
 
-app.use(express.json()) //tem que vir antes
+app.use(cors())
+app.use(express.json())
 app.use(routes)
 
 app.listen(3333)
-
-// Métodos http: get, post, put, delete
-
-// Tipos de parâmetros:
-
-// Query Params: request.query (filtros, ordenação, paginação, etc)
-// Route Params: request.params (identificar um recurso na alteração ou remoção)
-// Body: request.body (dados para criação ou alteração de um registro)
-
-// MongoDB (não-relacional)
