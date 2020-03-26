43501ad834c1efe2c3c2517bfabed9e0aabd2af9
Rafael Herzog <49111482+RafaelHerzog@users.noreply.github.com>
2020-03-25 21:44:41 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,10 +8,11 @@ const { setupWebsocket } = require('./websocket')
 
 const app = express()
 const server = http.Server(app)
+const connectionString = ''
 
 setupWebsocket(server)
 
-mongoose.connect('mongodb://omnistack:omnistack@cluster0-shard-00-00-brisu.mongodb.net:27017,cluster0-shard-00-01-brisu.mongodb.net:27017,cluster0-shard-00-02-brisu.mongodb.net:27017/week10?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
+mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
